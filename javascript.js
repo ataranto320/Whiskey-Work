8d4bf5834ecbbef5b64461bdf4ec87a6abcc750c
Austin T <austin.taranto@gmail.com>
2019-06-18 22:06:47 -0400

    added img slide html css javascript for later use

--- javascript.js
+++ javascript.js
@@ -1,1 +1,43 @@
 //on click function for drink imgs
+
+
+// will use at later date
+// start point
+// var i = 0;
+// var images = [];
+// var time = 8000;
+
+// //img lists
+
+// images[0] = 'assets/images/sunset.jpg';
+// images[1] = 'assets/images/village.jpg';
+// images[2] = 'assets/images/beachhouse.jpg';
+// images[3] = 'assets/images/oldworld.jpg';
+// images[4] = 'assets/images/southerntrees.jpg';
+// images[5] = 'assets/images/countryside.jpg';
+// images[6] = 'assets/images/winter.jpg';
+// images[7] = 'assets/images/townHD.jpg';
+// images[8] = 'assets/images/cityHD.jpg';
+
+
+
+
+// //change img
+// function changeImg() {
+//     var img = document.getElementById("slide")
+//     img.setAttribute("src", images[i]);
+//     img.className = "fadeIn";
+//     if (i < images.length - 1) {
+//         i++;
+//     } else {
+//         i = 0;
+//     }
+
+//     setTimeout(function () {
+//         img.className = "fadeOut";
+//     }, 4000);
+
+//     setTimeout("changeImg()", time);
+// }
+
+// window.onload = changeImg;
